bdcb7a61105b06e2c0124bf4a585f6d075ece3ea
gtkrshnaaa <gtkrishnacbspace@gmail.com>
2024-04-25 15:58:46 +0700

    update

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -2,7 +2,7 @@ function composeEmail() {
     const email = document.getElementById("email").value;
     const subject = document.getElementById("subject").value;
     const message = document.getElementById("message").value;
-    const receiverEmail = "gilangtejakrishnaa@gmail.com"; // Email penerima
+    const receiverEmail = "klinikumkm@gmail.com"; // Email penerima
 
     const mailtoLink = `mailto:${receiverEmail}?from=${encodeURIComponent(email)}&subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
 
